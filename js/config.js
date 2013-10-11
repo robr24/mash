40944a431f2d23a3a4fda14ec0f9c79d6fd8e500
Crystal Gardner <cgardner@weather.com>
2013-10-11 01:06:01 -0400

    added my latest -working on main.html (scrubber details)

---
+++ js/config.js
@@ -0,0 +1,36 @@
+window._skel_config = {
+	prefix: 'css/vendor/panel-style',
+	preloadStyleSheets: true,
+	resetCSS: false,
+	resetScroll: false,
+	swipeToClose: true,
+	boxModel: 'border',
+	grid: { gutters: 30 },
+	breakpoints: {
+		wide: { range: '1200-', containers: 1140, grid: { gutters: 50 } },
+		narrow: { range: '481-1199', containers: 960 },
+		narrower: { range: '481-960', containers: 'fluid' },
+		mobile: { range: '-480', containers: 'fluid', lockViewport: true, grid: { collapse: true } }
+	}
+};
+
+window._skel_panels_config = {
+	panels: {
+		bottomPanel: {
+			position: 'bottom',
+			size: "80%"
+			/*
+				Since this panel is a bit more complicated, we're omitting its 'html' option and
+				defining it inline (ie. in index.html).
+			*/
+		}
+	},
+	overlays: {
+		audioScrubber: {
+			position: 'bottom-center',
+			width: "100%",
+			height: 50
+//			html:	'<a href="#" class="toggle icon-envelope" data-action="togglePanel" data-args="bottomPanel">TOGGLE</a>'
+		}
+	}
+};
